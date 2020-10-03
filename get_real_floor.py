d29cec7c684cbfab63541ae51e128329f4278a23
Henry McCreery <henrymccy@gmail.com>
2020-10-02 19:37:18 -0700

    Update get_real_floor.py

--- get_real_floor.py
+++ get_real_floor.py
@@ -15,3 +15,13 @@ real_floor(7)
 real_floor(20)
 real_floor(1)
 real_floor(-6)
+
+def real_basement(basement):
+    basement_new=0
+    if i>basement>-13:
+        basement_new = basement+1
+    if basement<13:
+        basement_new = basement+2
+    if basement==-13:
+        basement_new = "there is no basement -13"
+    print(basement_new)
